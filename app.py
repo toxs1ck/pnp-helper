3994778ff7e6c3265db9cf55342132ea137fab15
toxs1ck <timotius97@googlemail.com>
2019-09-01 00:07:07 +0200

    Added Login page

--- app.py
+++ app.py
@@ -23,6 +23,12 @@ def register():
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
+            flash('You have been logged in!', 'success')
+            return redirect(url_for('index'))
+        else:
+            flash('Login Unsuccessful. Please check username and password', 'danger')
     return render_template('login.html', title='Login', form=form)
 
 
