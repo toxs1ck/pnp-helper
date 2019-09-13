ba638983375cff8db92fc7b4620799de90cf58dc
toxs1ck <timotius97@googlemail.com>
2019-09-13 23:47:15 +0200

    Added user page and Updating of user data

--- pnp/forms.py
+++ pnp/forms.py
@@ -1,7 +1,9 @@
 from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileAllowed
 from wtforms import StringField, PasswordField, SubmitField, BooleanField, DateField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from pnp.models import User
+from flask_login import current_user
 
 
 class RegistrationForm(FlaskForm):
@@ -27,14 +29,34 @@ class RegistrationForm(FlaskForm):
         if email:
             raise ValidationError('Email already in use.')
 
-    def validate_password(self, password, password_validation):
-        user = User.query.filter_by(usernem=username.data).first()
-        if user:
-            raise ValidationError('Validation Massage')
-
 
 class LoginForm(FlaskForm):
     email = StringField('Email', validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired()])
     remember = BooleanField('Remember Me')
     submit = SubmitField('Login')
+
+
+class UpdateAccountForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired(), Length(min=3, max=20)])
+    email = StringField('Email', validators=[DataRequired(), Email(), Length(max=120)])
+    confirm_email = StringField('Email', validators=[DataRequired(), Email()])
+
+    first_name = StringField('First Name', validators=[DataRequired(), Length(min=2, max=30)])
+    second_name = StringField('Second Name', validators=[DataRequired(), Length(min=2, max=40)])
+    birth_date = DateField('Birth date', validators=[DataRequired()])
+    picture = FileField('Update profile picture', validators=[FileAllowed(['jpg', 'png'])])
+
+    submit = SubmitField('Update')
+
+    def validate_username(self, username):
+        if username.data != current_user.username:
+            user = User.query.filter_by(username=username.data).first()
+            if user:
+                raise ValidationError('Username already taken. Please choose a different one.')
+
+    def validate_email(self, email):
+        if email.data != current_user.email:
+            email = Email.query.filter_by(email=email.data).first()
+            if email:
+                raise ValidationError('Email already in use.')
--- pnp/routes.py
+++ pnp/routes.py
@@ -1,6 +1,9 @@
+import secrets
+import os
+from PIL import Image
 from pnp import app, db, bcrypt
 from flask import render_template, flash, redirect, url_for, request
-from pnp.forms import RegistrationForm, LoginForm
+from pnp.forms import RegistrationForm, LoginForm, UpdateAccountForm
 from pnp.models import User, Game, Character
 from flask_login import login_user, current_user, logout_user, login_required
 
@@ -47,7 +50,42 @@ def logout():
     return redirect(url_for('index'))
 
 
-@app.route('/account')
+def save_picture(form_picture):
+    random_hex = secrets.token_hex(8)
+    _, f_ext = os.path.splitext(form_picture.filename)
+    picture_fn = random_hex + f_ext
+    picture_path = os.path.join(app.root_path, 'static/profile_pics', picture_fn)
+
+    output_size = (125, 125)
+    i = Image.open(form_picture)
+    i.thumbnail(output_size)
+
+    i.save(picture_path)
+
+    return picture_fn
+
+
+@app.route('/account', methods=['GET', 'POST'])
 @login_required
 def account():
-    return render_template('account.html', title='Account')
+    form = UpdateAccountForm()
+    if form.validate_on_submit():
+        if form.picture.data:
+            picture_file = save_picture(form.picture.data)
+            current_user.image_file = picture_file
+        current_user.username = form.username.data
+        current_user.email = form.email.data
+        current_user.first_name = form.first_name.data
+        current_user.second_name = form.second_name.data
+        current_user.birth_date = form.birth_date.data
+        db.session.commit()
+        flash('Your account has been updated.', 'success')
+        return redirect(url_for('account'))
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.email.data = current_user.email
+        form.first_name.data = current_user.first_name
+        form.second_name.data = current_user.second_name
+        form.birth_date.data = current_user.birth_date
+    image_file = url_for('static', filename='profile_pics/' + current_user.image_file)
+    return render_template('account.html', title='Account', image_file=image_file, form=form)
