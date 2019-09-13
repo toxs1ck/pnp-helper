a7c4ba72e9d2663429fafcc79f747479b6dc0053
toxs1ck <timotius97@googlemail.com>
2019-09-13 18:42:21 +0200

    User Authentication added

--- pnp/forms.py
+++ pnp/forms.py
@@ -1,11 +1,12 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField, DateField
-from wtforms.validators import DataRequired, Length, Email, EqualTo
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+from pnp.models import User
 
 
 class RegistrationForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired(), Length(min=2, max=20)])
-    email = StringField('Email', validators=[DataRequired(), Email(), Length(max=20)])
+    username = StringField('Username', validators=[DataRequired(), Length(min=3, max=20)])
+    email = StringField('Email', validators=[DataRequired(), Email(), Length(max=120)])
     confirm_email = StringField('Email', validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired()])
     confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
@@ -15,6 +16,21 @@ class RegistrationForm(FlaskForm):
     birth_date = DateField('Birth date', validators=[DataRequired()])
 
     submit = SubmitField('Sign Up')
+
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user:
+            raise ValidationError('Username already taken. Please choose a different one.')
+
+    def validate_email(self, email):
+        email = Email.query.filter_by(email=email.data).first()
+        if email:
+            raise ValidationError('Email already in use.')
+
+    def validate_password(self, password, password_validation):
+        user = User.query.filter_by(usernem=username.data).first()
+        if user:
+            raise ValidationError('Validation Massage')
 
 
 class LoginForm(FlaskForm):
--- pnp/routes.py
+++ pnp/routes.py
@@ -1,7 +1,8 @@
-from pnp import app
-from flask import render_template, flash, redirect, url_for
+from pnp import app, db, bcrypt
+from flask import render_template, flash, redirect, url_for, request
 from pnp.forms import RegistrationForm, LoginForm
 from pnp.models import User, Game, Character
+from flask_login import login_user, current_user, logout_user, login_required
 
 
 @app.route("/")
@@ -11,20 +12,42 @@ def index():
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
     form = RegistrationForm()
     if form.validate_on_submit():
-        flash(f'Account created for {form.username.data}!', 'success')
-        return redirect(url_for('index'))
+        hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+        user = User(username=form.username.data, email=form.email.data, password=hashed_password, first_name=form.first_name.data, second_name=form.second_name.data, birth_date=form.birth_date.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Your account has been created! You are now able to log in.', 'success')
+        return redirect(url_for('login'))
     return render_template('register.html', title='Registration', form=form)
 
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
     form = LoginForm()
     if form.validate_on_submit():
-        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
-            flash('You have been logged in!', 'success')
-            return redirect(url_for('index'))
+        user = User.query.filter_by(email=form.email.data).first()
+        if user and bcrypt.check_password_hash(user.password, form.password.data):
+            login_user(user, remember=form.remember.data)
+            next_page = request.args.get('next')
+            return redirect(next_page) if next_page else redirect(url_for('index'))
         else:
-            flash('Login Unsuccessful. Please check username and password', 'danger')
+            flash('Login Unsuccessful. Please check email and password', 'danger')
     return render_template('login.html', title='Login', form=form)
+
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
+
+@app.route('/account')
+@login_required
+def account():
+    return render_template('account.html', title='Account')
